7309a7ae041164815fc82c52c76b63d359b74c22
OmarZakaria10 <ok2m32@gmail.com>
2025-06-16 15:59:18 +0300

    restrict routes

--- routes/licenseRoutes.js
+++ routes/licenseRoutes.js
@@ -1,30 +1,15 @@
 const express = require("express");
 const licenseController = require("../controllers/licenseController");
 const validatedLicense = require("../middlewares/validateLicense");
+const authController = require('../controllers/authController');
 const router = express.Router();
 
-router.post(
-  "/createLicense",
-  validatedLicense.validateLicenseData,
-  licenseController.createLicense
-);
-
-// Get license by id
+router.use(authController.protect); // Protect all routes after this middleware
+router.use(authController.restrictTo('admin','user','license','viewer')); // Restrict all routes 
 router.get("/getLicenseById", licenseController.getLicenseById);
-//Update License
-router.patch("/updateLicense/:id", licenseController.updateLicense);
-// Delete License
-// Delete a license by ID
-router.delete("/deleteLicense/:id", licenseController.deleteLicense);
-
 // Get all licenses
 router.get("/getAllLicenses", licenseController.getAllLicenses);
-// Get all licenses with associated vehicles
-router.get(
-  "/getAllLicensesWithVehicles",
-  licenseController.getAllLicensesWithVehicles
-);
-
+// Get license by vehicle ID
 router.get(
   "/getLicenseByVehicleId/:id",
   licenseController.getLicenseByVehicleId
@@ -33,8 +18,26 @@ router.get(
 router.get("/getExpiredLicenses", licenseController.getExpiredLicenses);
 // Get unique field values for filtering
 router.get("/getUniqueFieldValues", licenseController.getUniqueFieldValues);
-
 // Get expiring licenses
 router.get("/getExpiringLicenses", licenseController.getExpiringLicensesBefore);
+
+router.use(authController.restrictTo('admin','user','license',)); // Restrict all routes 
+router.post(
+  "/createLicense",
+  validatedLicense.validateLicenseData,
+  licenseController.createLicense
+);
+
+//Update License
+router.patch("/updateLicense/:id", licenseController.updateLicense);
+// Delete License
+// Delete a license by ID
+router.delete("/deleteLicense/:id", licenseController.deleteLicense);
+
+// Get all licenses with associated vehicles
+router.get(
+  "/getAllLicensesWithVehicles",
+  licenseController.getAllLicensesWithVehicles
+);
 
 module.exports = router;
--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const vehicleController = require("../controllers/vehicleController");
+const authController = require('../controllers/authController');
 const router = express.Router();
 const Vehicle = require("../models/vehicleModel");
 
-router.post("/createVehicle", vehicleController.createVehicle);
+router.use(authController.protect); // Protect all routes after this middleware
+router.use(authController.restrictTo('admin','user','GPS','viewer')); // Restrict all routes to admin, user, vehicle, and viewer roles
+router.get("/getUniqueFieldValues", vehicleController.getUniqueFieldValues);
 router.get("/getAllVehicles", vehicleController.getAllVehicles);
 router.get("/getVehicle/:id", vehicleController.getVehicleById);
 router.get("/getFilteredVehicles", vehicleController.getFilteredVehicles);
-router.patch("/updateVehicle/:id", vehicleController.updateVehicle);
-router.delete("/deleteVehicle/:id", vehicleController.deleteVehicle);
-router.get("/getUniqueFieldValues", vehicleController.getUniqueFieldValues);
 router.get("/getVehiclesBySector/:sector", vehicleController.getVehiclesBySector);
 router.get("/getVehiclesByAdminstration/:adminstration", vehicleController.getVehiclesByAdministration);
+
+router.use(authController.restrictTo('admin','user','GPS')); 
+router.post("/createVehicle", vehicleController.createVehicle);
+router.patch("/updateVehicle/:id", vehicleController.updateVehicle);
+
+router.use(authController.restrictTo('admin','user',)); 
+router.delete("/deleteVehicle/:id", vehicleController.deleteVehicle);
+
 
 module.exports = router;
